0b7451adb9851a0f3374a283e7617e9ff5a070f9
franco555 <razon20m@gmail.com>
2019-04-11 23:27:01 -0300

    Modificado table, editado buscar y paginacion

--- PPSale/App_Start/BundleConfig.cs
+++ PPSale/App_Start/BundleConfig.cs
@@ -24,15 +24,20 @@ namespace PPSale
                       "~/Scripts/respond.js",
                       "~/Scripts/moment.js",
                       "~/Scripts/fa-jquery-confirm.js",
+                      "~/Scripts/fa-jquery.dataTables.min.js",
+                      "~/Scripts/fa-dataTables.bootstrap4.min.jss",
                       "~/Scripts/fa-bootstrap-datetimepicker.js",
+                      "~/Scripts/fa-FunctionBase.js",
                       "~/Scripts/fa-fileUpload.js",
                       "~/Scripts/fa-NavBar.js",
-                      "~/Scripts/fa-ECommerce.js"));
+                      "~/Scripts/fa-ECommerce.js")); 
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css",
                       "~/Content/fa-jquery-confirm.css",
+                      "~/Content/fa-dataTable.bootstrap4.min.css",
                       "~/Content/fa-bootstrap-datetimepicker.css",
                       "~/Content/fa-fileUpload.css",
                       "~/Content/fa-ECommerceMenu.css",
---
+++ PPSale/Scripts/fa-FunctionBase.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    $('#example').DataTable({
+        "language": {
+            "sProcessing": "Procesando...",
+            "sLengthMenu": '<div class="input-group mb-2"><div class="input-group-prepend"><div class="input-group-text"> Mostrar</div></div> _MENU_  </div>',
+            "sZeroRecords": "No se encontraron resultados",
+            "sEmptyTable": "Ningún dato disponible en esta tabla",
+            "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sUrl": "",
+            "sInfoThousands": ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            }
+        }
+    });
+});
